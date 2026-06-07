9b6b9552fa1f4d8115aa366485e802629721433b
Fyonietz <expduta@gmail.com>
2026-06-07 18:03:09 +0700

    Solving Tile Formatting Problem

--- src/engine.zig
+++ src/engine.zig
@@ -1,7 +1,7 @@
 const Video = @import("video.zig");
-
+pub const Tile_Format = [7][2]u32;
 pub const Object = struct {
     Id:u32 = 0,
-    Tile:[7][8]u8,
+    Tile:Tile_Format,
     Map:[*]volatile Video.Tile
 };
--- src/renderer.zig
+++ src/renderer.zig
@@ -5,39 +5,32 @@ pub inline fn rgb_to_gba(r:u5,g:u5,b:u5) u16{
     return @as(u16,r) | (@as(u16,g ) << 5) | (@as(u16,b) << 10);
 }
 
-fn packed_byte(counter:u5,arg:u8)u32{
-    if(counter == 0){
-        return (@as(u32,arg));
-    }
-    if(counter == 1){
-        return (@as(u32,arg) << 8);
-    }
-    if(counter == 2){
-        return (@as(u32,arg) << 16);
-    }
-    if(counter == 3){
-        return (@as(u32,arg) << 24);
-    }
-
-    return 0 ;
+fn reverse_endian(data:anytype) u32{
+    return (@as(u32,data & 0x000000FF) << 24) |
+           (@as(u32,data & 0x0000FF00) << 8) |
+           (@as(u32,data & 0x00FF0000) >> 8) |
+           (@as(u32,data & 0xFF000000) >> 24);
 }
 
 pub fn draw_pixel(obj:Engine.Object) void{
-    var block:u32 = 0;
-    while(block < 16):(block+=1){
-        var column_counter:u5 = 0;
-        if(block % 2 == 0){
-            column_counter = 0 ;
-            while(column_counter < 4) : (column_counter+=1){
-                var row_counter:u5 = 0;
-                while(row_counter < 7) : (row_counter+=1){
-                    obj.Map[obj.Id][block] =  packed_byte(column_counter,obj.Tile[row_counter][column_counter]);
-                }
-            }
-        }else{
+    // var block:u32 = 0;
+    // while(block < 16):(block+=1){
+    //     var column_counter:u5 = 0;
+    //     if(block % 2 == 0){
+    //         column_counter = 0 ;
+    //         while(column_counter < 4) : (column_counter+=1){
+    //             var row_counter:u5 = 0;
+    //             while(row_counter < 7) : (row_counter+=1){
+    //                 obj.Map[obj.Id][block] =  packed_byte(row_counter,column_counter,obj.Tile);
+    //             }
+    //         }
+    //     }else{
+    //
+    //     }
+    // }
 
-        }
-    }
+    obj.Map[obj.Id][0]=reverse_endian(obj.Tile[0][0]);
+    obj.Map[obj.Id][1]=reverse_endian(obj.Tile[0][1]);
 }
 
 //Target
